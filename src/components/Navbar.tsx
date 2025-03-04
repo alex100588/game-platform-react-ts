f8b77ac89ada54426ea79ec27dbe7e8378ba507e
alex100588 <alexandru_ionasc@yahoo.com>
2025-03-04 16:31:40 +0200

    test2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ interface Props{
 const Navbar = ({onSearch}: Props) => {
   return (
     <HStack padding='12px'>
-        <Image src={logo} boxSize="60px"></Image>
+        <Image boxSize="60px"></Image>
         <SearchInput onSearch={onSearch} />
         <ColorModeSwitch />
     </HStack>
